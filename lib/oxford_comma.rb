cfa4e5b1be92db408a2690c07f5771eab090c9db
Anya Brun <tsbrun@icloud.com>
2020-07-21 05:46:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+elsif array.length == 2
+    array.join(" and ")
+  else
+    # add commas plus a final 'and'
+    # access last element of the array and put an 'and' in front of it
+    # then join with commas
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
