f50c1445698e241efca7545b13ca5d83f84becc3
Anya Brun <tsbrun@icloud.com>
2020-07-21 05:44:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
-    one_element_array = array.join
-  end
-  #array.join(", ")
-  if array.length == 2
+    array.join
+elsif array.length == 2
     array.join(" and ")
   end
 end
